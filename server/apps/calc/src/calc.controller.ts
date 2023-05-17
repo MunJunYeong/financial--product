67985303cb2f8e56f8828d219447d25021270579
Junyeong Mun <junyeong.mun@corelinesoft.com>
2023-05-17 16:35:13 +0900

    자유적립, 정기 적금 DTO 및 swagger 추가

--- server/apps/calc/src/calc.controller.ts
+++ server/apps/calc/src/calc.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { CalcService } from './calc.service';
+import { ApiOkResponse, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Calculate')
+@ApiResponse({
+  status: 500,
+  description: 'Internal server error',
+})
 @Controller()
 export class CalcController {
   constructor(private readonly calcService: CalcService) {}
 
-  @Get()
-  getHello(): string {
-    return this.calcService.getHello();
+  // Ping-Pong Test
+  @ApiOperation({ summary: 'calc ping pong test' })
+  @ApiOkResponse({
+    description: '핑퐁 성공 유무',
+    schema: {
+      example: { success: "pong" },
+    },
+  })
+  @Get('/ping')
+  testPingPong() {
+    return 'pong';
   }
+  
 }
--- server/apps/calc/src/savings/dto/common.dto.ts
+++ server/apps/calc/src/savings/dto/common.dto.ts
@@ -1,27 +1,65 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
-import { IsNumber, IsBoolean, IsString, IsDate } from "class-validator";
+import { IsNumber, IsBoolean, IsString, IsDate } from 'class-validator';
 
-// prepayment
-export class PrePostPayment {
-    @IsNumber()
-    period: number // 적금 기간
+// 선납이연
+export class PrePostPaymentDTO {
+  @IsNumber()
+  period: number; // 적금 기간
 
-    @IsString()
-    payMethod: string // 납입방식
+  @IsString()
+  payMethod: string; // 납입방식
 
-    @IsNumber()
-    price: number // 월 납입액
+  @IsNumber()
+  price: number; // 월 납입액
 
-    @IsNumber()
-    rate: number // 금리
+  @IsNumber()
+  rate: number; // 금리
 
-    @IsBoolean()
-    isSimple: boolean //  true : 단리,   false : 복리
+  @IsBoolean()
+  isSimple: boolean; //  true : 단리,   false : 복리
 
-    @Type(()=> Date)
-    @IsDate()
-    startDate: Date // 가입 일
+  @Type(() => Date)
+  @IsDate()
+  startDate: Date; // 가입 일
 
-    @IsNumber()
-    interest: number // 이자세율
+  @IsNumber()
+  interest: number; // 이자세율
+}
+
+export class CalcSavingsOutputDTO {
+  @ApiProperty({
+    example: '100000',
+    description: '예치 총 금액',
+  })
+  @IsNumber()
+  totalAmount: number;
+
+  @ApiProperty({
+    example: '83754',
+    description: '일반(15.4) 세후 이자',
+  })
+  @IsNumber()
+  generalInterest: number;
+  
+  @ApiProperty({
+    example: '89595',
+    description: '세금우대 (9.5%)	 세후 이자',
+  })
+  @IsNumber()
+  taxPreferentialInterest: number;
+
+  @ApiProperty({
+    example: '97614',
+    description: '세금우대 (1.4%) 세후 이자',
+  })
+  @IsNumber()
+  taxPreferentialInterest2: number;
+  
+  @ApiProperty({
+    example: '99000',
+    description: '비과세 (0%)',
+  })
+  @IsNumber()
+  taxFreeInterest: number;
 }
