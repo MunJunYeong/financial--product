9aa66c6bf316b3a666ae3d8ba57d1bcb54818732
Junyeong Mun <junyeong.mun@corelinesoft.com>
2023-07-26 09:38:56 +0900

    로그인 기능 추가

--- server/apps/auth/src/user/user.service.ts
+++ server/apps/auth/src/user/user.service.ts
@@ -1,16 +1,21 @@
 // vendor
 import { HttpService } from '@nestjs/axios';
 import { Injectable } from '@nestjs/common';
-import bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
 
 // cus
 import { UserRepo } from './user.repo';
-import { LoginInputDTO, SignUpDTO } from './dto/common.dto';
+import { LoginInputDTO, LoginOutputDTO, SignUpDTO } from './dto/common.dto';
 import { User } from '@app/database/models/user';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
-  constructor(private readonly httpService: HttpService, private readonly userRepo: UserRepo) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly userRepo: UserRepo,
+    private jwtService: JwtService,
+  ) {}
 
   async SaveUser(data: SignUpDTO) {
     let res: any;
@@ -26,22 +31,29 @@ export class UserService {
     return res;
   }
 
-  async Login(data: LoginInputDTO) {
+  async Login(data: LoginInputDTO): Promise<LoginOutputDTO | Boolean> {
     let targetUser: User = null;
 
     try {
       targetUser = await this.userRepo.FindUserByID(data.id);
-
-      if (targetUser === null){
-        return 
-      }
     } catch (err) {
       throw err;
     }
 
-    if (data.password === targetUser.pw) {
-      
+    if (targetUser === null) {
+      return false;
+    }
+    
+    const isMatch = await bcrypt.compare(data.password, targetUser.pw);
+    if (!isMatch) {
+      return false;
     }
 
+    // Sequelize 모델 인스턴스가 나오기에 data만 추출
+    const payload = targetUser.dataValues
+    return {
+      accessToken: this.jwtService.sign(payload),
+      refreshToken: this.jwtService.sign(payload),
+    };
   }
 }
